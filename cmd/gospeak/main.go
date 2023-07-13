8a793cc03dda516789647a8f38a3cacf8de2226d
Vojtech Vitek <vojtech.vitek@golang.cz>
2023-07-13 16:03:31 +0200

    Parse //go:webrpc comments and execute each target for given interface

--- cmd/gospeak/main.go
+++ cmd/gospeak/main.go
@@ -14,7 +14,7 @@ var (
 )
 
 func main() {
-	schemaDir, targets, err := collectCliArgs(os.Args[1:])
+	schemaDir, _, err := collectCliArgs(os.Args[1:])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "%v\n\n", err)
 		fmt.Fprintf(os.Stderr, usage)
@@ -26,56 +26,56 @@ func main() {
 		os.Exit(1)
 	}
 
-	schema, err := gospeak.Parse(schemaDir, "")
+	targets, err := gospeak.Parse(schemaDir)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to parse Go schema: %v\n", err)
 		os.Exit(1)
 	}
 
 	if len(targets) == 0 {
-		fmt.Fprintf(os.Stderr, "target is required: try gospeak --help\n")
+		fmt.Fprintf(os.Stderr, "no interfaces with //go:webrpc found, see https://github.com/golang-cz/gospeak\n")
 		os.Exit(1)
 	}
 
 	for _, target := range targets {
-		if target.name == "json" {
-			jsonSchema, _ := schema.ToJSON()
-			if err := os.WriteFile(target.out, []byte(jsonSchema), 0644); err != nil {
-				fmt.Fprintf(os.Stderr, "failed to write to %q file: %v\n", target.out, err)
+		if target.Generator == "json" {
+			jsonSchema, _ := target.Schema.ToJSON()
+			if err := os.WriteFile(target.OutFile, []byte(jsonSchema), 0644); err != nil {
+				fmt.Fprintf(os.Stderr, "failed to write to %q file: %v\n", target.OutFile, err)
 				os.Exit(1)
 			}
-			fmt.Printf("generated %v ✓\n", target.out)
+			fmt.Printf("interface %v{} => %v ✓\n", target.InterfaceName, target.OutFile)
 			continue
 		}
 
 		config := &gen.Config{
 			RefreshCache:    false,
 			Format:          true,
-			TemplateOptions: target.opts,
+			TemplateOptions: target.Opts,
 		}
 
-		generated, err := gen.Generate(schema, target.name, config)
+		generated, err := gen.Generate(target.Schema, target.Generator, config)
 		if err != nil {
 			fmt.Fprintln(os.Stderr, err.Error())
 			os.Exit(1)
 		}
 
-		if err := os.WriteFile(target.out, []byte(generated.Code), 0644); err != nil {
-			fmt.Fprintf(os.Stderr, "failed to write to %q file: %v\n", target.out, err)
+		if err := os.WriteFile(target.OutFile, []byte(generated.Code), 0644); err != nil {
+			fmt.Fprintf(os.Stderr, "failed to write to %q file: %v\n", target.OutFile, err)
 			os.Exit(1)
 		}
-		fmt.Printf("generated %v ✓\n", target.out)
+		fmt.Printf("interface %v{} => %v ✓\n", target.InterfaceName, target.OutFile)
 	}
 }
 
-type target struct {
-	name string
-	out  string
-	opts map[string]interface{}
+type Target struct {
+	Name string
+	Out  string
+	Opts map[string]interface{}
 }
 
 // gospeak <schema.go> <target> [-targetOpts...] -out=<file> ... [<targetN> [-targetOpts] -out=<file>...]
-func collectCliArgs(args []string) (schema string, targets []*target, err error) {
+func collectCliArgs(args []string) (schema string, targets []*Target, err error) {
 	for i, arg := range args {
 		// CLI flags or target options
 		if strings.HasPrefix(arg, "-") {
@@ -106,7 +106,7 @@ func collectCliArgs(args []string) (schema string, targets []*target, err error)
 }
 
 // <target> [-targetOpts...] ... [<targetN> [-targetOpts]...]
-func collectTargets(args []string) (targets []*target, err error) {
+func collectTargets(args []string) (targets []*Target, err error) {
 	currentTarget := -1
 
 	for _, arg := range args {
@@ -118,22 +118,22 @@ func collectTargets(args []string) (targets []*target, err error) {
 
 			// target options
 			if name == "out" {
-				targets[currentTarget].out = value
+				targets[currentTarget].Out = value
 			} else {
-				targets[currentTarget].opts[name] = value
+				targets[currentTarget].Opts[name] = value
 			}
 		} else {
 			currentTarget++
-			targets = append(targets, &target{
-				name: name,
-				opts: map[string]interface{}{},
+			targets = append(targets, &Target{
+				Name: name,
+				Opts: map[string]interface{}{},
 			})
 		}
 	}
 
 	for i, target := range targets {
-		if target.out == "" {
-			return nil, fmt.Errorf("target[%v] %v must have -out=<path> flag", i, target.name)
+		if target.Out == "" {
+			return nil, fmt.Errorf("target[%v] %v must have -out=<path> flag", i, target.Name)
 		}
 	}
 
@@ -141,25 +141,27 @@ func collectTargets(args []string) (targets []*target, err error) {
 }
 
 const usage = `
-Usage: gospeak <schema.go> <target> [-targetOpts...] -out=<file> ...
+Usage: gospeak <schema.go>
   -h, --help
         print this help
   -v, --version
         print gospeak version and exit
 
-Targets:
-  json
-  golang       (see https://github.com/webrpc/gen-golang)
-  typescript   (see https://github.com/webrpc/gen-typescript)
-  javascript   (see https://github.com/webrpc/gen-javascript)
-  openapi      (see https://github.com/webrpc/gen-openapi)
+Finds all Go interfaces annotated with the special //go:webrpc target command comment.
+Creates Webrpc schema from the Go interface.
+Executes webrpc code generation for the given targets.
 
-Example usage:
-  gospeak path/to/api.go json -out=api.json
+Example:
 
-  gospeak ./schema/api.go                                  \
-    json -out ./schema.json                                \
-    golang -server -pkg server -out ./server/server.gen.go \
-    golang -client -pkg client -out ./client/client.gen.go \
-    typescript -client -out ../frontend/src/client.gen.ts  \
+package api
+
+//go:webrpc golang@v0.11.0 -client -out=../client.gen.go
+//go:webrpc typescript@v0.11.0 -client -out=../client.gen.ts
+type ExampleAPI interface {
+	Ping(context.Context) (*Pong, error)
+}
+
+type Pong struct {
+	Message string
+}
 `
--- parser.go
+++ parser.go
@@ -2,6 +2,8 @@ package gospeak
 
 import (
 	"fmt"
+	"go/ast"
+	"go/token"
 	"go/types"
 	"os"
 	"path/filepath"
@@ -12,8 +14,16 @@ import (
 	"golang.org/x/tools/go/packages"
 )
 
+type Target struct {
+	Schema        *schema.WebRPCSchema
+	Generator     string
+	InterfaceName string
+	OutFile       string
+	Opts          map[string]interface{}
+}
+
 // Parse Go source file or package folder and return WebRPC schema.
-func Parse(filePath string, goInterfaceName string) (*schema.WebRPCSchema, error) {
+func Parse(filePath string) ([]*Target, error) {
 	path, err := filepath.Abs(filePath)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to get directory from %q", path)
@@ -29,72 +39,139 @@ func Parse(filePath string, goInterfaceName string) (*schema.WebRPCSchema, error
 	}
 
 	cfg := &packages.Config{
-		Dir:  path,
-		Mode: packages.NeedName | packages.NeedImports | packages.NeedTypes | packages.NeedFiles | packages.NeedDeps | packages.NeedSyntax,
+		Dir: path,
+		Mode: packages.NeedName | packages.NeedImports | packages.NeedTypes | packages.NeedFiles | packages.NeedDeps | packages.NeedSyntax | packages.LoadSyntax | packages.NeedTypes | packages.NeedTypesInfo | packages.NeedName | packages.NeedFiles | packages.NeedCompiledGoFiles |
+			packages.NeedImports | packages.NeedDeps |
+			packages.NeedTypes | packages.NeedSyntax | packages.NeedTypesInfo,
 	}
 
-	schemaPkg, err := packages.Load(cfg, path)
+	pkgs, err := packages.Load(cfg, path)
 	if err != nil {
 		return nil, errors.Wrapf(err, "failed to load Go packages from %q", path)
 	}
-	if len(schemaPkg) != 1 {
-		return nil, errors.Errorf("failed to load Go package (len=%v) from %q", len(schemaPkg), path)
+	if len(pkgs) != 1 {
+		return nil, errors.Errorf("failed to load Go package (len=%v) from %q", len(pkgs), path)
+	}
+	pkg := pkgs[0]
+
+	// Collect Go interfaces with `//go:webrpc` comments.
+	targets, err := collectInterfaces(pkg)
+	if err != nil {
+		return nil, errors.Wrapf(err, "collecting Go interfaces")
 	}
 
-	p := &parser{
-		schema: &schema.WebRPCSchema{
-			WebrpcVersion: "v1",
-			SchemaName:    goInterfaceName,
-			SchemaVersion: "",
-		},
-		schemaPkgName:   schemaPkg[0].Name,
-		parsedTypes:     map[types.Type]*schema.VarType{},
-		parsedTypeNames: map[string]struct{}{},
+	cache := map[string]*schema.WebRPCSchema{}
+	for _, target := range targets {
+		if interfaceSchema, ok := cache[target.InterfaceName]; ok {
+			// Hit.
+			target.Schema = interfaceSchema
+		}
 
-		// TODO: Change this to map[*types.Package]string so we can rename duplicated pkgs?
-		importedPaths: map[string]struct{}{
-			// Initial schema file's package name artificially set by golang.org/x/tools/go/packages.
-			"command-line-arguments": {},
-		},
-	}
+		// Miss.
+		p := &parser{
+			schema: &schema.WebRPCSchema{
+				WebrpcVersion: "v1",
+				SchemaName:    target.InterfaceName,
+				SchemaVersion: "",
+			},
+			schemaPkgName:   pkg.Name,
+			parsedTypes:     map[types.Type]*schema.VarType{},
+			parsedTypeNames: map[string]struct{}{},
 
-	scope := schemaPkg[0].Types.Scope()
+			// TODO: Change this to map[*types.Package]string so we can rename duplicated pkgs?
+			importedPaths: map[string]struct{}{
+				// Initial schema file's package name artificially set by golang.org/x/tools/go/packages.
+				"command-line-arguments": {},
+			},
+		}
 
-	for _, goInterfaceName := range listInterfaces(scope) {
-		obj := scope.Lookup(goInterfaceName)
+		scope := pkg.Types.Scope()
+
+		obj := scope.Lookup(target.InterfaceName)
 		if obj == nil {
-			return nil, errors.Errorf("interface %q not found (list of interfaces: %v)", goInterfaceName, strings.Join(listInterfaces(scope), ", "))
+			return nil, errors.Errorf("type interface %v{} not found", target.InterfaceName)
 		}
 
 		iface, ok := obj.Type().Underlying().(*types.Interface)
 		if !ok {
-			return nil, errors.Errorf("%q is %T (list of interfaces: %v)", goInterfaceName, obj.Type().Underlying(), strings.Join(listInterfaces(scope), ", "))
+			return nil, errors.Errorf("type %v{} is %T", target.InterfaceName, obj.Type().Underlying())
 		}
 
-		if err := p.parseInterfaceMethods(iface, goInterfaceName); err != nil {
-			return nil, errors.Wrapf(err, "failed to parse interface %q", goInterfaceName)
+		if err := p.parseInterfaceMethods(iface, target.InterfaceName); err != nil {
+			return nil, errors.Wrapf(err, "failed to parse interface %q", target.InterfaceName)
 		}
+
+		target.Schema = p.schema
+		cache[target.InterfaceName] = p.schema
 	}
 
-	return p.schema, nil
+	return targets, nil
 }
 
-func listInterfaces(scope *types.Scope) []string {
-	interfaces := []string{}
+// Find all Go interfaces with the special //go:webrpc comments.
+func collectInterfaces(pkg *packages.Package) ([]*Target, error) {
+	var targets []*Target
 
-	for _, name := range scope.Names() {
-		obj := scope.Lookup(name)
-		if obj == nil {
-			continue
-		}
-
-		_, ok := obj.Type().Underlying().(*types.Interface)
-		if ok {
-			interfaces = append(interfaces, name)
+	for _, file := range pkg.Syntax {
+		for _, decl := range file.Decls {
+			if typeDeclaration, ok := decl.(*ast.GenDecl); ok && typeDeclaration.Tok == token.TYPE {
+				for _, spec := range typeDeclaration.Specs {
+					if typeSpec, ok := spec.(*ast.TypeSpec); ok {
+						if _, ok := typeSpec.Type.(*ast.InterfaceType); ok {
+							doc := typeDeclaration.Doc
+							if doc != nil {
+								for _, comment := range doc.List {
+									if webrpcCmd, hasPrefix := strings.CutPrefix(comment.Text, "//go:webrpc "); hasPrefix {
+										target, err := parseWebrpcCommand(webrpcCmd)
+										if err != nil {
+											return nil, fmt.Errorf("failed to parse %s", comment.Text)
+										}
+										target.InterfaceName = typeSpec.Name.Name
+										targets = append(targets, target)
+									}
+								}
+							}
+						}
+					}
+				}
+			}
 		}
 	}
 
-	return interfaces
+	return targets, nil
+}
+
+// Parses webrpc CLI command into a target, ie. webrpc typescript@v0.11.0 -client -out=./videoAuthoringClient.gen.ts.
+func parseWebrpcCommand(cmd string) (*Target, error) {
+	target := &Target{
+		Opts: map[string]interface{}{},
+	}
+
+	for _, arg := range strings.Split(cmd, " ") {
+		name, value, _ := strings.Cut(arg, "=")
+
+		if strings.HasPrefix(name, "-") {
+			name = strings.TrimLeft(name, "-")
+
+			// target options
+			if name == "out" {
+				target.OutFile = value
+			} else {
+				target.Opts[name] = value
+			}
+		} else {
+			if target.Generator != "" {
+				return nil, fmt.Errorf("unexpected argument %v", name)
+			}
+			target.Generator = name
+		}
+	}
+
+	if target.OutFile == "" {
+		return nil, fmt.Errorf("-out=<path> flag is required")
+	}
+
+	return target, nil
 }
 
 // Parses Go source file and returns WebRPC schema.
