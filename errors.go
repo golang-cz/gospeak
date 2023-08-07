2c02d7033725c08dc1ba8510697e6798b2f6d6a9
Vojtech Vitek <vojtech.vitek@golang.cz>
2023-08-07 14:54:29 +0200

    Experimental: Embed webrpc errors source code

---
+++ errors.go
@@ -0,0 +1,55 @@
+package gospeak
+
+import (
+	"errors"
+	"fmt"
+)
+
+// Deprecated: This type may be removed in the future.
+type WebRPCError struct {
+	Name       string `json:"error"`
+	Code       int    `json:"code"`
+	Message    string `json:"msg"`
+	Cause      string `json:"cause,omitempty"`
+	HTTPStatus int    `json:"status"`
+	cause      error
+}
+
+var _ error = WebRPCError{}
+
+func (e WebRPCError) Error() string {
+	if e.cause != nil {
+		return fmt.Sprintf("%s %d: %s: %v", e.Name, e.Code, e.Message, e.cause)
+	}
+	return fmt.Sprintf("%s %d: %s", e.Name, e.Code, e.Message)
+}
+
+func (e WebRPCError) Is(target error) bool {
+	if rpcErr, ok := target.(WebRPCError); ok {
+		return rpcErr.Code == e.Code
+	}
+	return errors.Is(e.cause, target)
+}
+
+func (e WebRPCError) Unwrap() error {
+	return e.cause
+}
+
+func ErrorWithCause(rpcErr WebRPCError, cause error) WebRPCError {
+	err := rpcErr
+	err.cause = cause
+	err.Cause = cause.Error()
+	return err
+}
+
+// Webrpc errors
+var (
+	ErrWebrpcEndpoint      = WebRPCError{Code: 0, Name: "WebrpcEndpoint", Message: "endpoint error", HTTPStatus: 400}
+	ErrWebrpcRequestFailed = WebRPCError{Code: -1, Name: "WebrpcRequestFailed", Message: "request failed", HTTPStatus: 400}
+	ErrWebrpcBadRoute      = WebRPCError{Code: -2, Name: "WebrpcBadRoute", Message: "bad route", HTTPStatus: 404}
+	ErrWebrpcBadMethod     = WebRPCError{Code: -3, Name: "WebrpcBadMethod", Message: "bad method", HTTPStatus: 405}
+	ErrWebrpcBadRequest    = WebRPCError{Code: -4, Name: "WebrpcBadRequest", Message: "bad request", HTTPStatus: 400}
+	ErrWebrpcBadResponse   = WebRPCError{Code: -5, Name: "WebrpcBadResponse", Message: "bad response", HTTPStatus: 500}
+	ErrWebrpcServerPanic   = WebRPCError{Code: -6, Name: "WebrpcServerPanic", Message: "server panic", HTTPStatus: 500}
+	ErrWebrpcInternalError = WebRPCError{Code: -7, Name: "WebrpcInternalError", Message: "internal error", HTTPStatus: 500}
+)
